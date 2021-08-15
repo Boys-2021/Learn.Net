df92cec1e3d495c700ee24c441a7000ccec20644
unknown <adrian.20191176@iit.ac.lk>
2021-08-15 21:12:59 +0530

    ServcieReponse change

--- RolePlay/Services/CharacterService/ServiceResponse.cs
+++ RolePlay/Services/CharacterService/ServiceResponse.cs
@@ -4,6 +4,6 @@ namespace RolePlay.Services.CharacterService
     {
         public T Data {get; set;}
         public bool Success {get; set; }= true;
-        public string Message {get; set;} = null;
+        public string Message {get; set;} = "Success Dude";
     }
 }
